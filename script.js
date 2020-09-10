7c5a944568ef7432960224cff28172f1d0da2e0d
Connor Dillon <connor@codewithconnor.com>
2020-09-10 14:19:37 -0700

    :+1:  Implemented local storage functionality

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const titleSelect = document.querySelector('#movie')
 let ticketPrice = +titleSelect.value // using `+` (alternatively ParseInt()) to force typeof to be `number`
 // console.log(typeof ticketPrice)
 
+//$ Save selected title and price to localstorage
+function setTitleData(titleData, titlePrice) {
+  localStorage.setItem('selectedTitleIndex', titleData)
+  localStorage.setItem('selectedTitlePrice', titlePrice)
+}
+
 //$ Update total and count
 /**
  * Updates the DOM with the number of selected seats and the corresponding price of those tickets
@@ -16,6 +22,16 @@ let ticketPrice = +titleSelect.value // using `+` (alternatively ParseInt()) to 
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected')
   //   console.log(selectedSeats) // Test to see if the DOM is correctly tracking the number of selected seats
+
+  // Copy selected seats into arr
+  // Map through array
+  // return a new array with indexes
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat)
+  })
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
   const selectedSeatsCount = selectedSeats.length
   //   console.log(selectedSeatsCount) // Test to see if the DOM is correctly tracking the number of selected seats
   count.innerText = selectedSeatsCount
@@ -27,6 +43,7 @@ function updateSelectedCount() {
 //$ Title select event
 titleSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value
+  setTitleData(e.target.selectedIndex, e.target.value)
   updateSelectedCount()
 })
 
